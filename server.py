b13ec8dde4a55a7a35c9319f59af83dce97ef097
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-11 14:42:23 -0400

    added session initializations and assignments. linked to html and sucessfully randomized an addition to the session

--- server.py
+++ server.py
@@ -4,5 +4,37 @@ app.secret_key='ivegotasecretforyou...boo!'
 import random
 @app.route('/')
 def index():
-	return render_template('index.html')
+	if not session.has_key('gold'):
+		session['gold'] = 0
+	if not session.has_key('farm'):
+		session['farm'] = 0
+	if not session.has_key('cave'):
+		session['cave'] = 0
+	if not session.has_key('house'):
+		session['house'] = 0
+	if not session.has_key('casino'):
+		session['casino'] = 0
+	gold = session['gold'] + session['farm'] + session['cave'] + session['house'] + session['casino']
+
+
+
+
+	print session
+	print 'You have', gold, 'gold'
+	return render_template('index.html', gold=gold)
+
+
+
+
+
+
+
+@app.route('/process_money', methods=['POST'])
+def money():
+	if request.form['farm_gold']:
+		session['farm'] += random.randint(10,20)
+
+	return redirect('/')
+
+
 app.run(debug=True)
